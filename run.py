491566d67d82622fbd7b7a231884eacd8ae6a3cf
arthurmezaonik <arthur.mezaonik@gmail.com>
2021-07-28 07:00:31 +0000

    Create a function that check if the user is already registered and validate the answear

--- run.py
+++ run.py
@@ -12,20 +12,45 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('coders_bistro')
 
+
 class Customer:
     "Create a new customer"
-    def __init__ (self, f_name, l_name, email):
-        #instance attibutes
+    def __init__(self, f_name, l_name, email):
+        # Instance attibutes
         self.f_name = f_name
         self.l_name = l_name
         self.email = email
-    
+
     def info(self):
-        return f'Thaks for visiting us {self.f_name} {self.l_name}. Your order was sent on {self.email} email.'
+        return f'Thaks for visiting us {self.f_name} {self.l_name}. Your order was sent on {self.email}'
 
-customer = Customer("Arthur", "Martins", "arthur.mezaonik@gmail.com")
-print(customer.info())
 
-def run():
-    print("Welcome to the Coders Bistro.\n")
-    print("Are you already registered?\n")
+def is_registred():
+    """
+    Check if the user is already registered
+    """
+    while True:
+        print("Are you registered (Y / N)?")
+        first_answear = input("Enter your answear here:\n")
+
+        if validate_is_registred(first_answear):
+            print("Valid answear")
+            break
+
+    return first_answear
+
+
+def validate_is_registred(data):
+    """
+    Validate the answear for the is_registered question
+    """
+    try:
+        answear = data.strip().lower()
+        if answear not in ("y", "n"):
+            raise ValueError('Please choose between Y or N')
+
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
